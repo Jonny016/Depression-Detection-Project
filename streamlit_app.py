f2a4edbc5ca7f21be6d9cf38d829d3f0b2556096
Vishal Kumbhar <vishalkumbhar7910@gmail.com>
2025-04-21 02:56:41 +0530

    Re

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,11 @@ import numpy as np
 from keras.models import load_model
 from keras.preprocessing.image import img_to_array
 import gdown
-import os
+import osfrom streamlit_webrtc import webrtc_streamer
+
+st.title("Webcam Stream Example")
+
+webrtc_streamer(key="example")
 
 # Constants
 labels = ['Not Depressed', 'Depressed']
